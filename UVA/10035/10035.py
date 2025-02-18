3a076d0ad210d92bbd4978764d339cc9d4863676
Crystal <chi200706@gmail.com>
2025-02-18 14:42:18 +0800

    進位邏輯修正

--- UVA/10035/10035.py
+++ UVA/10035/10035.py
@@ -6,7 +6,7 @@
 # python 10035.py < 1.in > 1.out
 while True:
   line = input()
-  line_list = line.split(' ')
+  line_list = line.split() # domjudge空格問題
   num = int(line_list[0])
   next_num = int(line_list[1])
 
@@ -14,20 +14,25 @@ while True:
     break
 
   carry = 0
-  while (num > 0 and next_num > 0):
-    if num % 10 + next_num % 10 >= 10:
-      carry += 1
-      num = num // 10
-      next_num = next_num // 10 + 1
-    else:
+  carry_count = 0
+  while num > 0 or next_num > 0:
+    if num % 10 + next_num % 10 + carry >= 10:
+      carry_count += 1
+      carry = 1
       num = num // 10
       next_num = next_num // 10
-  if carry == 1:
+    else:
+      carry = 0
+      num = num // 10
+      next_num = next_num // 10
+
+  if carry_count == 1:
     print('1 carry operation.') # Python預設就會換行了
-  elif carry == 0:
+  elif carry_count == 0:
     print('No carry operation.')
   else:
-    print(str(carry) + ' carry operations.')
+    print(str(carry_count) + ' carry operations.')
+    
 
 # No carry operation.
 # 3 carry operations.
