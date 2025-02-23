12c6abb37b61d8fb6671a324866ac357956d8e02
chi200706 <chi200706@gmail.com>
2025-02-23 14:34:42 +0000

    更新10035寫法

--- UVA/10035/10035.py
+++ UVA/10035/10035.py
@@ -5,33 +5,29 @@
 # 0 0
 # python 10035.py < 1.in > 1.out
 while True:
-  line = input()
-  line_list = line.split() # domjudge空格問題
-  num = int(line_list[0])
-  next_num = int(line_list[1])
+    line = input()
+    if line == '0 0':
+        break
+    line_arr = list(map(int, line.split()))
+    num = line_arr[0]
+    next_num = line_arr[1]
+    carry = 0
+    carry_time = 0
+    while num > 0 or next_num > 0:
+        if num % 10 + next_num % 10 + carry >= 10:
+            carry_time += 1
+            carry = 1
+        else:
+            carry = 0
+        num = num // 10
+        next_num = next_num // 10
 
-  if num == 0 and next_num == 0: # stdin真的要注意型別欸==
-    break
-
-  carry = 0
-  carry_count = 0
-  while num > 0 or next_num > 0:
-    if num % 10 + next_num % 10 + carry >= 10:
-      carry_count += 1
-      carry = 1
-      num = num // 10
-      next_num = next_num // 10
+    if carry_time == 0:     
+        print('No carry operation.')
+    elif carry_time == 1:
+        print('1 carry operation.')
     else:
-      carry = 0
-      num = num // 10
-      next_num = next_num // 10
-
-  if carry_count == 1:
-    print('1 carry operation.') # Python預設就會換行了
-  elif carry_count == 0:
-    print('No carry operation.')
-  else:
-    print(str(carry_count) + ' carry operations.')
+        print(f'{carry_time} carry operations.')
     
 
 # No carry operation.
