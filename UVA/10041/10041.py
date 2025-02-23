9be0dc1932b4706caa0d709369a8c274b6905b3a
chi200706 <chi200706@gmail.com>
2025-02-23 14:59:12 +0000

    更新10041寫法

--- UVA/10041/10041.py
+++ UVA/10041/10041.py
@@ -3,25 +3,15 @@
 # 2 2 4
 # 3 2 4 6
 # 4 2 1 999 5
+# python 10041.py < 1.in > 1.out
 num = int(input())
-cal_list = []
-for i in range(0, num):
-    test_group = input()
-    cal_list.append(list(map(int, test_group.split())))
-
-for e in cal_list:
-  mid_index = e[0] // 2
-
-  ne_list = e[1:]
-  ne_list.sort()
-
-  mid = int(ne_list[mid_index])
-
-  line_ans = 0
-  for sub_e in ne_list:
-    temp_d = int(sub_e) - mid
-    line_ans += abs(temp_d)
-  print(line_ans)
+for _ in range(num):
+    line = list(map(int, input().split()))
+    mid_index = line[0] // 2
+    ne_list = line[1:]
+    mid = ne_list[mid_index]     
+    ans = sum(abs(x - mid) for x in ne_list)
+    print(ans)
 
 # 2 
 # 4
