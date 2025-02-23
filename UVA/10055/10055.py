119a9384459a520c25acbc8e8a260a30b3e3a687
chi200706 <chi200706@gmail.com>
2025-02-23 14:37:29 +0000

    更新10055註解

--- UVA/10055/10055.py
+++ UVA/10055/10055.py
@@ -6,7 +6,7 @@
 while True: # 外面要有一個迴圈才能用break
   try:
     line = input()
-    line = line.split(' ')
+    line = line.split(' ') # 每題測資空格不一樣？這題為什麼這樣寫系統就給過
     print(abs(int(line[0]) - int(line[1])))
   except EOFError:
     break
