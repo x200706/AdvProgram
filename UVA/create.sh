e4bae5c16bb6158698b8b108a56a479014d4548b
chi200706 <chi200706@gmail.com>
2025-02-24 09:01:24 +0000

    shell改為傳參

--- UVA/create.sh
+++ UVA/create.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# 定義資料夾名稱列表
-folders=("folder1" "folder2" "folder3" "folder4")
+# 檢查是否有傳入參數
+if [ $# -eq 0 ]; then
+    echo "請提供資料夾名稱作為參數。"
+    echo "使用方法: $0 folder1 folder2 folder3 ..."
+    exit 1
+fi
 
-# 遍歷資料夾列表
-for folder in "${folders[@]}"; do
+# 遍歷所有傳入的參數（資料夾名稱）
+for folder in "$@"; do
     # 創建資料夾
     mkdir -p "$folder"
     
