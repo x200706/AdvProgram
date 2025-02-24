12e18f91790db7845c9f33797a2bb6e8b4d63e1a
Crystal <99391710+x200706@users.noreply.github.com>
2025-02-24 14:07:47 +0800

    美化一下100.35排版跟變數名稱

--- UVA/10035/10035.py
+++ UVA/10035/10035.py
@@ -9,25 +9,27 @@ while True:
     if line == '0 0':
         break
     line_arr = list(map(int, line.split()))
+
     num = line_arr[0]
     next_num = line_arr[1]
     carry = 0
-    carry_time = 0
+    carry_count = 0
+    
     while num > 0 or next_num > 0:
         if num % 10 + next_num % 10 + carry >= 10:
-            carry_time += 1
+            carry_count += 1
             carry = 1
         else:
             carry = 0
         num = num // 10
         next_num = next_num // 10
 
-    if carry_time == 0:     
+    if carry_count == 0:     
         print('No carry operation.')
-    elif carry_time == 1:
+    elif carry_count == 1:
         print('1 carry operation.')
     else:
-        print(f'{carry_time} carry operations.')
+        print(f'{carry_count} carry operations.')
     
 
 # No carry operation.
