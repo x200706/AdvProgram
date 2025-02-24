9e44b22eb3be03fadfd6e3f3c61a9628b67bc26c
chi200706 <chi200706@gmail.com>
2025-02-24 09:56:32 +0000

    初步寫法..

--- UVA/100/100.py
+++ UVA/100/100.py
@@ -1,21 +1,32 @@
-line_arr = list(map(int, input().split()))
-line_arr.sort()
-start = line[0]
-end = line[1]
-max_time = 0
-table = {}
-# 直到EOF
-for i in range(start, end + 1):
-    cal(i)
-    
-def cal(n):
-    if n == 1:
+table = {}  
+while True:
+    try:
+        line_arr = list(map(int, input().split()))
+        line_arr.sort()
+        start = line_arr[0]
+        end = line_arr[1]
+        max_time = 0
+        time = 0
+        for i in range(start, end + 1):
+            n_for_deal = i
+            time = 0
+            while True:
+                if n_for_deal in table: # 如果計算過了
+                    time += table[n_for_deal]
+                    break
+                
+                time += 1
+                if n_for_deal == 1:
+                    break
+                if n_for_deal % 2 == 0:
+                    n_for_deal = n_for_deal // 2
+                else:
+                    n_for_deal = 3 * n_for_deal + 1
+            table[i] = time
+            max_time = max(time, max_time)
+        print(f'{start} {end} {max_time}')
+    except EOFError:
         break
-    # if n % 2 == 0:
-    #      n // 2
-    # else:
-    #      3 * n + 1
 
-
-print(f'{start} {end} {max_time}')
-# 參考 https://github.com/jlhung/UVA-Python/blob/master/100%20-%20The%203n%20%2B%201%20problem.py
+# 參考
+# https://blog.iddle.dev/public/2023/03/19/Python-UVa-100-The-3n-1-problem/#google_vignette 無遞迴解
