f73f99cbbb63120fc08e01903697d47ce016c567
chi200706 <chi200706@gmail.com>
2025-02-25 15:56:12 +0000

    10008寫到一半

--- UVA/10008/10008.py
+++ UVA/10008/10008.py
@@ -1,1 +1,14 @@
 # 解題思路：正則表達式留下英文 然後全轉大寫登記進table 算次數
+import re
+
+table = {}
+
+while True:
+    try:
+        line = input()
+        line_after_regex = re.sub('[^a-zA-Z]', '', line)
+        line_list = list(line_after_regex.upper())
+        print(line_list)
+        #sort dict
+    except EOFError:
+        break
