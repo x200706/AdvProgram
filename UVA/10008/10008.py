75d589dcd284fc2dc4fa923aa8b22e38b7caba5a
chi200706 <chi200706@gmail.com>
2025-02-26 10:26:29 +0000

    完成10008題

--- UVA/10008/10008.py
+++ UVA/10008/10008.py
@@ -1,4 +1,4 @@
-# 解題思路：正則表達式留下英文 然後全轉大寫登記進table 算次數
+# 解題思路：正則表達式留下英文 然後全轉大寫登記進table計次數
 import re
 
 table = {}
@@ -8,7 +8,14 @@ while True:
         line = input()
         line_after_regex = re.sub('[^a-zA-Z]', '', line)
         line_list = list(line_after_regex.upper())
-        print(line_list)
-        #sort dict
+        for e in line_list:
+            if e in table.keys():
+                table[e] += 1
+            else:
+                table[e] = 1
     except EOFError:
         break
+
+for k, v in sorted(table.items(), key = lambda x : (-x[1], x[0])):
+    print(k, v)
+# FYI https://gdst.dev/posts/UVA-10008/
