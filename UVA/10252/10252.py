033e05d46bdbbdf8aedfa808c95cac1c84b770a1
chi200706 <chi200706@gmail.com>
2025-03-03 09:41:40 +0000

    完成10252

--- UVA/10252/10252.py
+++ UVA/10252/10252.py
@@ -9,21 +9,23 @@ while True:
     try:
         str1_arr = list(input())
         str2_arr = list(input())
-        # same_arr = []
-        same_dict = {}
+        str1_dict = {}
+        str2_dict = {}
         for e in str1_arr:
-            for l in str2_arr:
-                if e == l and e in same_dict:
-                    # same_arr.append(e)
-                    same_dict[e] += 1
-                elif e == l and e not in same_dict:
-                    same_dict[e] = 1
-        print(same_dict)
-        # same_arr = list(set(same_arr))
-        # same_arr.sort()
-        # for e in same_arr:
-        #     print(e, end='')
+            str1_dict[e] = str1_dict.get(e, 0) + 1 # 這樣就不用寫很長的判斷了
+        for e in str2_arr:
+            str2_dict[e] = str2_dict.get(e, 0) + 1 # 這樣就不用寫很長的判斷了
+        ans = []
+        for k in str1_dict.keys():
+            if k in str2_dict:
+                time = min(str1_dict[k], str2_dict[k])
+                for i in range(time):
+                    ans.append(k)
+        ans.sort()
+        for e in ans:
+            print(e, end = '')
         print('')
+
     except EOFError:
         break
 # FYI https://blog.iddle.dev/public/2023/05/23/Python-UVa-10252-Common-Permutation/
