7f22d6350eacb27e5adeef59107de8356f34e88f
Crystal <chi200706@gmail.com>
2025-03-16 23:07:58 +0800

    修改10038寫法

--- UVA/10038/10038.py
+++ UVA/10038/10038.py
@@ -1,25 +1,26 @@
 while True:
     try:
         line_arr = list(map(int, input().split()))
-        num = line_arr[0]
+        n = line_arr[0]
 
         diff_arr = []
-        for i in range(num):
-            if i == 0:
-                continue
+        for i in range(1, n):
             diff_arr.append(abs(line_arr[i] - line_arr[i + 1]))
         diff_arr.sort()
 
         flag = True
-
-        for i in range(num):
-            if i == 0:
-                continue
+        # print(n) -> 4
+        # print(len(diff_arr)) -> 3
+        # print(diff_arr) -> [1, 2, 3]
+        for i in range(1, n): #HINT 這邊為甚麼是N是個好問題
+            # print(i) -> 1 2 3
             if diff_arr[i - 1] != i:
                 flag = False
                 break
 
-        ans = "Jolly" if flag else "Not jolly"
-        print(ans)
+        if flag is True:
+            print('Jolly')
+        else:
+            print('Not Jolly')
     except EOFError:
         break
