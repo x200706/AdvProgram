566a95fe287b84fe5e7e6d9f42774632c8b21587
chi200706 <chi200706@gmail.com>
2025-03-17 10:12:09 +0000

    修好10812

--- UVA/10812/10812.py
+++ UVA/10812/10812.py
@@ -2,11 +2,15 @@ n = int(input())
 for _ in range(n):
     num1, num2 = map(int, input().split())
     flag = True
-    if (num1 % 2 != 0) or (num1 < num2):
-        flag = False
+
+    # 提前終止
+    if num2 > num1 or ((num1 + num2) % 2 != 0):
+        print('impossible')
+        continue
+
     a = (num1 + num2) / 2
     b = a - num2
-    if b < 0 or flag is False:
+    if b < 0:
         print('impossible')
     else:
         print(f'{int(a)} {int(b)}')
