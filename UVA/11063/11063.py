27faedd0d95af20b0ae9e0a4cc84695380049f1b
chi200706 <chi200706@gmail.com>
2025-03-17 11:23:17 +0000

    11063改了但還是WA

--- UVA/11063/11063.py
+++ UVA/11063/11063.py
@@ -7,17 +7,30 @@ while True:
             continue
         n = int(line)
         arr = list(map(int, input().split()))
-        
-        cal_arr = []
-        flag = True
+        flag = True #HINT 旗子不要一直插錯位置rrr
+        #HINT 條件一：數列前項小於等於後項
+        for i in range(1, n):
+            if arr[i - 1] >= arr[i]:
+                flag = False
+                break
+        if flag is False:
+            ans = 'It is not a B2-Sequence.'
+            print(f'Case #{I}: {ans}')
+            I += 1
+            continue
 
-        for i in arr:
-            for j in arr:
-                su = i + j
+        #HINT 條件二：所有相加的可能不能出現一樣的數字
+        # 加過的不要再加避免誤判—巢狀迴圈要避免重複計算
+        cal_arr = []
+        for i in range(n):
+            for j in range(i, n):
+                su = arr[i] + arr[j]
                 if su in cal_arr:
                     flag = False
-                    break  # ans怪怪的欸
-                cal_arr.append(sum)
+                    break
+                cal_arr.append(su)
+            if not flag: # 留意巢狀迴圈跳出..
+                break
 
         ans = ''
         if flag is True:
@@ -28,3 +41,16 @@ while True:
         I += 1
     except EOFError:
         break
+
+# 找規律
+# 巢狀迴圈對應arr索引號
+# 0 0
+# 0 1
+# 0 2
+# 0 3
+# 1 1
+# 1 2
+# 1 3
+# 2 2
+# 2 3
+# 3 3
